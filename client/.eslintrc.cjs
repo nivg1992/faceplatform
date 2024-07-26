d71a9c78888c6719e786ab9d9c0a20217431673a
tamir.halima <tamir.halima@appsflyer.com>
2024-07-26 18:15:51 +0300

    prettier format

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,31 +5,28 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "plugin:prettier/recommended"
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs','build','node_modules'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'build', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh','import'],
+  plugins: ['react-refresh', 'import'],
   rules: {
-    "no-console": [
-      "error",
+    'no-console': [
+      'error',
       {
-        "allow": ["error","warn","info"]
+        allow: ['error', 'warn', 'info']
       }
     ],
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "no-use-before-define": "warn",
-    "react-hooks/exhaustive-deps": "error",
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "import/order": [
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-use-before-define': 'warn',
+    'react-hooks/exhaustive-deps': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/order': [
       2,
       {
-        "groups": ["builtin", "external", "internal", ["parent", "sibling", "index"]],
-        "newlines-between": "always"
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always'
       }
-    ],
-  },
-}
+    ]
+  }
+};
